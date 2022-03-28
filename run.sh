09809abe8b72393450cae5bd2550131d8fcd626f
MorxTheGamer <zamarwint11@gmail.com>
2022-03-28 18:56:07 +0000

    Mod Bot

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ git config --global user.name "MorxTheGamer"
 git config --global user.email "zamarwint11@gmail.com"
 git remote add origin "https://github.com/morxthegamer/morxthegamer.git"
 git add .
-git commit -m "Utilities Bot"
-git checkout -b "utilities"
-git push --set-upstream origin utilities
+git commit -m "Mod Bot"
+git checkout -b "moderation"
+git push --set-upstream origin moderation
